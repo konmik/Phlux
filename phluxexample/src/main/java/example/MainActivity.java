f7978dfe9b9b7d2d28706da3f3a932d14eeb4ae3
konmik <morelk@gmail.com>
2016-01-04 16:50:23 +0300

    constants in MainState

--- phluxexample/src/main/java/example/MainActivity.java
+++ phluxexample/src/main/java/example/MainActivity.java
@@ -13,9 +13,6 @@ import phlux.Transient;
 
 public class MainActivity extends PhluxActivity<MainState> {
 
-    private static final String NAME_1 = "Chuck Norris";
-    private static final String NAME_2 = "Jackie Chan";
-    private static final String DEFAULT_NAME = NAME_1;
     private static final int REQUEST_ID = 1;
 
     CheckedTextView check1;
@@ -31,17 +28,17 @@ public class MainActivity extends PhluxActivity<MainState> {
         check1 = (CheckedTextView) findViewById(R.id.check1);
         check2 = (CheckedTextView) findViewById(R.id.check2);
 
-        check1.setText(NAME_1);
-        check2.setText(NAME_2);
+        check1.setText(MainState.NAME_1);
+        check2.setText(MainState.NAME_2);
 
-        check1.setOnClickListener(v -> switchTo(NAME_1));
-        check2.setOnClickListener(v -> switchTo(NAME_2));
+        check1.setOnClickListener(v -> switchTo(MainState.NAME_1));
+        check2.setOnClickListener(v -> switchTo(MainState.NAME_2));
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter = new ArrayAdapter<>(this, R.layout.item));
 
         if (savedInstanceState == null)
-            background(REQUEST_ID, Request.create(DEFAULT_NAME), true);
+            background(REQUEST_ID, Request.create(MainState.DEFAULT_NAME), true);
     }
 
     private void switchTo(final String name) {
@@ -53,14 +50,14 @@ public class MainActivity extends PhluxActivity<MainState> {
 
     @Override
     protected MainState initial() {
-        return MainState.create(DEFAULT_NAME);
+        return MainState.create();
     }
 
     @Override
     protected void update(MainState state) {
         part("tabs", state.name(), name -> {
-            check1.setChecked(name.equals(NAME_1));
-            check2.setChecked(name.equals(NAME_2));
+            check1.setChecked(name.equals(MainState.NAME_1));
+            check2.setChecked(name.equals(MainState.NAME_2));
         });
         part("items", state.items(), items -> {
             adapter.clear();
--- phluxexample/src/main/java/example/MainState.java
+++ phluxexample/src/main/java/example/MainState.java
@@ -7,15 +7,20 @@ import phlux.Transient;
 
 @AutoParcel
 public abstract class MainState implements PhluxState {
+
+    static final String NAME_1 = "Chuck Norris";
+    static final String NAME_2 = "Jackie Chan";
+    static final String DEFAULT_NAME = NAME_1;
+
     public abstract String name();
     public abstract Transient<String> error();
     public abstract Transient<ServerAPI.Item[]> items();
 
     public abstract Builder toBuilder();
 
-    public static MainState create(String name) {
+    public static MainState create() {
         return new AutoParcel_MainState.Builder()
-            .name(name)
+            .name(DEFAULT_NAME)
             .error(new Transient<>())
             .items(new Transient<>())
             .build();
